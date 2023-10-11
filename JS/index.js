847f041b468a6d887f8e4014004d8369e7b8c43e
yusup02 <53394689+yusup02@users.noreply.github.com>
2023-10-11 21:58:04 +0300

    ed

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,11 @@
 if (navigator.userAgent.match(/Android TV/)) {
     // Определено, что клиент использует Android TV
     // Показываем кнопку "Скачать"
-    document.querySelector('.download-button').style.display = 'block';
-} 
+    document.querySelector('.islamApp').style.display = 'block';
+} else if (navigator.userAgent.match(/Android/))  {
+    window.location.href = "https://play.google.com/store/apps/details?id=slam.islamapp&hl=ru&gl=US";
+} else if (navigator.userAgent.match(/iOS/)) {
+    window.location.href = "https://apps.apple.com/ru/app/islamapp-%D0%B2%D1%80%D0%B5%D0%BC%D1%8F-%D0%BD%D0%B0%D0%BC%D0%B0%D0%B7%D0%B0-%D0%BA%D0%B8%D0%B1%D0%BB%D0%B0/id1452368807";
+} else if (navigator.userAgent.match(/Macintosh/)) {
+    window.location.href = "https://apps.apple.com/ru/app/islamapp-%D0%B2%D1%80%D0%B5%D0%BC%D1%8F-%D0%BD%D0%B0%D0%BC%D0%B0%D0%B7%D0%B0-%D0%BA%D0%B8%D0%B1%D0%BB%D0%B0/id1452368807"; 
+}
