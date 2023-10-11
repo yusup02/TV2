ce85dc18bc62a44f4a41f25df2b8e486e8ccf9e7
yusup02 <53394689+yusup02@users.noreply.github.com>
2023-10-11 11:51:41 +0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+if (navigator.userAgent.match(/Android TV/)) {
+    // Определено, что клиент использует Android TV
+    // Показываем кнопку "Скачать"
+    document.querySelector('.download-button').style.display = 'block';
+} 
